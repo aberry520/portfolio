de09a847ec12969c6545933928d908ece33b9a1e
Austin Berry <aberry52098@gmail.com>
2023-11-25 22:49:23 -0500

    add earth and moon

--- main.js
+++ main.js
@@ -17,22 +17,24 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(80);
 // renderer.render(scene, camera);
 
-const geometry = new THREE.SphereGeometry(3, 16, 100);
+const earthtexture = new THREE.TextureLoader().load("images/earth2.jpeg");
+const geometry = new THREE.SphereGeometry(10, 50, 50);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xf7771e,
+  // color: 0xf7771e,
+  map: earthtexture,
   // wireframe: true,
-  emissive: 0xf7771e,
-  emissiveIntensity: 0.3,
+  emissive: 0xffffff,
+  emissiveIntensity: 0.01,
 });
 const ambientLight = new THREE.AmbientLight(0xffffff);
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(0, 0, 0);
-pointLight.intensity = 300;
+pointLight.position.set(-50, 0, 0);
+pointLight.intensity = 2000;
 
-ambientLight.intensity = 0.5;
+ambientLight.intensity = 0.2;
 scene.add(pointLight, ambientLight);
 const lighthelper = new THREE.PointLightHelper(pointLight);
 scene.add(lighthelper);
@@ -55,11 +57,23 @@ function addStars() {
   scene.add(star);
 }
 const stars = Array(400).fill().forEach(addStars);
+
+// const spacetexture = new THREE.TextureLoader().load("images/spacebg.png");
+// scene.background = spacetexture;
+const moontexture = new THREE.TextureLoader().load("images/moon.jpg");
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(2.5, 50, 50),
+  new THREE.MeshStandardMaterial({
+    map: moontexture,
+  })
+);
+moon.position.set(20, 30, 20);
+scene.add(moon);
 function animate() {
   requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  // torus.rotation.y += 0.005;
-  // torus.rotation.z += 0.01;
+  // torus.rotation.x += 0.01;
+  torus.rotation.y += 0.001;
+  // torus.rotation.z += 0.0001;
   // scene.rotation.x += 0.01;
   scene.rotation.y += 0.0005;
   // scene.rotation.z += 0.01;
