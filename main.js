01d15da703686bfa3ec0783c4f4fbcfc166eca82
Austin Berry <aberry52098@gmail.com>
2023-11-25 21:24:57 -0500

    add stars and change shape to sphere

--- main.js
+++ main.js
@@ -20,16 +20,19 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 // renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const geometry = new THREE.SphereGeometry(3, 16, 100);
 const material = new THREE.MeshStandardMaterial({
   color: 0xf7771e,
   // wireframe: true,
+  emissive: 0xf7771e,
+  emissiveIntensity: 0.3,
 });
 const ambientLight = new THREE.AmbientLight(0xffffff);
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(0, 0, 0);
-pointLight.intensity = 100;
-ambientLight.intensity = 0.25;
+pointLight.intensity = 300;
+
+ambientLight.intensity = 0.5;
 scene.add(pointLight, ambientLight);
 const lighthelper = new THREE.PointLightHelper(pointLight);
 scene.add(lighthelper);
@@ -37,11 +40,29 @@ const torus = new THREE.Mesh(geometry, material);
 const controls = new OrbitControls(camera, renderer.domElement);
 scene.add(torus);
 // renderer.render(scene, camera);
+
+function addStars() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+  });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+const stars = Array(400).fill().forEach(addStars);
 function animate() {
   requestAnimationFrame(animate);
   torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  // torus.rotation.y += 0.005;
+  // torus.rotation.z += 0.01;
+  // scene.rotation.x += 0.01;
+  scene.rotation.y += 0.0005;
+  // scene.rotation.z += 0.01;
   controls.update();
   renderer.render(scene, camera);
 }
